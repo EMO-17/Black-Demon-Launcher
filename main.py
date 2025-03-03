424c4f8eeef1ad9e736cc7f7f67e30627d92185c
EMO-17 <eugenis198@gmail.com>
2025-03-03 17:13:41 -0400

    cambin el nombre del archivo principal

--- main.py
+++ main.py
@@ -1,23 +1,117 @@
-import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+import minecraft_launcher_lib
+import subprocess
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+# Diretorio de instalacion..
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+#variables..
+version_minecraft = "fabric-loader-0.16.10-1.17.1"
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+current_max = 0
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
 
-        self.button.clicked.connect(self.magic)
+def set_status(status: str):
+    print(status)
 
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+
+def set_progress(progress: int):
+    if current_max != 0:
+        print(f"{progress}/{current_max}")
+
+
+def set_max(new_max: int):
+    global current_max
+    current_max = new_max
+
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+
+callback = {
+    "setStatus": set_status,
+    "setProgress": set_progress,
+    "setMax": set_max
+}
+
+
+
+#funciones de intalacion...
+def instalar_version():
+    #instalar version de minecraft...
+    minecraft_launcher_lib.install.install_minecraft_version(version_minecraft, minecraft_directory, callback=callback)
+
+def instalar_version_fabric():
+    #instalar version de minecraft con fabric...
+    minecraft_launcher_lib.fabric.install_fabric(version_minecraft, minecraft_directory, callback=callback)
+
+
+#funciones de ejecucion...
+def ejecutar():
+    print("Ejecutando..")
+    
+    #options = minecraft_launcher_lib.utils.generate_test_options()  
+    options = {
+        "username": "EMO_17",
+        "uuid": "The UUID",
+        "token": "The acces token"
+        }
+
+    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version_minecraft, minecraft_directory, options)
+    
+    subprocess.run(minecraft_command, cwd=minecraft_directory)
+
+
+def verificar_versiones():
+    # Obtener las versiones instaladas
+    installed_versions = minecraft_launcher_lib.utils.get_installed_versions(minecraft_directory)
+
+    # Imprimir las versiones instaladas
+    print("Versiones de Minecraft instaladas:")
+    for version in installed_versions:
+        print(version)
+
+
+
+
+
+
+#Interfaz del emulador ...
+
+print("Que accion realisaras?")
+print("  1) Intalar una version...")
+print("  2) Intalar fabric")
+print("  3) Ejecutar una version...")
+
+op = input(">>> ")
+
+
+try:
+    op = int(op)
+    
+    if op == 1:
+        version_minecraft=input("Que version deseas intalar? :")
+        instalar_version()
+
+    elif op == 2:
+        version_minecraft=input("A que version de minecraft deseas intalale el fablric? :")
+        instalar_version_fabric()
+        
+    elif op == 3:
+        verificar_versiones()
+        version_minecraft=input("Que version de minecraft deseas ejecutar? (Ej: 1.17.1) :")
+        ejecutar()
+
+    else:
+        print("No elegiste nada....",op)
+
+except ValueError:
+    print("Entrada no válida. Por favor, ingresa un número.")
+
+
+
+
+
+
+    
+
+
+
